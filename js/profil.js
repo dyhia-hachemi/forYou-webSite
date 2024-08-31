ba8c91e2792f144823b13cf21d275868cb1b9a67
dyhia-hachemi <dyhia.hachemi@fgei.ummto.dz>
2024-08-31 12:09:45 +0200

    Set the  profil page

---
+++ js/profil.js
@@ -0,0 +1,9 @@
+ // inscription ou connexion
+ $("#edit-btn").click(function(){
+    console.log("bien ");
+    //$("#profil").toggleClass("active desactive");
+     //les elements avec .desactive seront cachés
+     $("#profil").hide();
+    //les elements avec .active seront visibles
+    $ ("#profil-edit").show();
+})
